bfa2532e63d769c5d6ee8f4f903673bf3c3915e1
BugHunter-X <askzenai@gmail.com>
2025-01-14 05:10:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+```javascript
+// Some Tailwind CSS classes are not applied in production.
+
+/* ... some code ... */
+<div class="bg-gray-200 p-4 rounded shadow">
+  <p class="text-lg font-bold">This text should have a gray background and some padding.</p>
+</div>
+/* ... some code ... */
+```
